82d2a3e0208ecd1c1f72d4f81ecbcfd106d4b1ea
Paweł Suder <pawel.suder94@gmail.com>
2024-10-24 13:33:43 +0200

    task 05 done

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
 class Textarea extends React.Component {
+
+    getTextAreaHeight = () => {
+        return this.textarea.offsetHeight
+    }
+
+    getScrollTextAreaHeight = () => {
+        return this.textarea.scrollHeight
+    }
+
+    componentDidUpdate(props, state, snapshot) {
+        console.log(this.getTextAreaHeight(), this.getScrollTextAreaHeight())
+
+        if (snapshot.resize && this.getTextAreaHeight() < this.getScrollTextAreaHeight()) {
+            this.textarea.style.height = `${this.getScrollTextAreaHeight()}px`
+        }
+    }
+
+    getSnapshotBeforeUpdate = () => {
+        if (this.getTextAreaHeight() < 100) {
+            return { resize: true }
+        } else {
+            return { resize: false }
+        }
+    }
+
     render() {
-        return <textarea></textarea>
+        const { content, handleChange } = this.props
+
+        return (
+            <textarea
+                onChange={() => handleChange(this.textarea.value)}
+                ref={el => this.textarea = el}
+                value={content}
+            />
+        )
     }
 }
 
